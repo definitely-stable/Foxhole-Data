c38421ef3f24665d5a62fccdd18ffac17bc00523
MrFr3di <ynagi-wm@mail.ru>
2026-09-20 20:58:56 +0500

    feat: register testable War API transport boundary

--- src/FoxData.Worker/Program.cs
+++ src/FoxData.Worker/Program.cs
@@ -13,6 +13,8 @@ var warApiOptions = WarApiWorkerOptions.FromConfiguration(
 builder.Services.AddSingleton(warApiOptions);
 builder.Services.AddSingleton(TimeProvider.System);
 builder.Services.AddSingleton<WarApiTransport>();
+builder.Services.AddSingleton<IWarApiTransport>(
+    serviceProvider => serviceProvider.GetRequiredService<WarApiTransport>());
 builder.Services.AddScoped<WarApiRegistryResolver>();
 
 builder.Services.AddHostedService<BootstrapWorker>();
