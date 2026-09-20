52dd7d4a720dbb331248919bc62aea1f266185c4
MrFr3di <ynagi-wm@mail.ru>
2026-09-20 22:31:30 +0500

    feat: version scheduling with collection profile

--- src/FoxData.Sources.WarApi/WarApiVersions.cs
+++ src/FoxData.Sources.WarApi/WarApiVersions.cs
@@ -6,4 +6,11 @@ public static class WarApiVersions
     public const string Parser = "warapi-parser@1";
     public const string CachePolicy = "warapi-cache-policy@1";
     public const string PollPolicy = "warapi-poll@1";
+
+    public static string SchedulingPolicy(
+        WarApiCollectionProfile collectionProfile)
+    {
+        ArgumentNullException.ThrowIfNull(collectionProfile);
+        return $"{PollPolicy}/{collectionProfile.Version}";
+    }
 }
