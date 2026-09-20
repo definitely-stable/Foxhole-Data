04badc41364093c5aef8f22ea1a65dd4e0dedd6b
MrFr3di <ynagi-wm@mail.ru>
2026-09-20 20:55:14 +0500

    feat: version durable War API poll policy

--- src/FoxData.Sources.WarApi/WarApiVersions.cs
+++ src/FoxData.Sources.WarApi/WarApiVersions.cs
@@ -5,4 +5,5 @@ public static class WarApiVersions
     public const string Adapter = "warapi-adapter@1";
     public const string Parser = "warapi-parser@1";
     public const string CachePolicy = "warapi-cache-policy@1";
+    public const string PollPolicy = "warapi-poll@1";
 }
