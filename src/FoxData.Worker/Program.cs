2232ae29d3310a48ca115698d30a308c72d3f4a9
MrFr3di <ynagi-wm@mail.ru>
2026-09-20 20:56:50 +0500

    feat: wire M3 bootstrap recovery planner and executor

--- src/FoxData.Worker/Program.cs
+++ src/FoxData.Worker/Program.cs
@@ -6,6 +6,18 @@ var builder = Host.CreateApplicationBuilder(args);
 
 builder.AddFoxDataTelemetry(FoxDataServiceNames.Worker);
 builder.Services.AddFoxDataInfrastructure(builder.Configuration);
+
+var warApiOptions = WarApiWorkerOptions.FromConfiguration(
+    builder.Configuration);
+
+builder.Services.AddSingleton(warApiOptions);
+builder.Services.AddSingleton(TimeProvider.System);
+builder.Services.AddSingleton<WarApiTransport>();
+builder.Services.AddScoped<WarApiRegistryResolver>();
+
 builder.Services.AddHostedService<BootstrapWorker>();
+builder.Services.AddHostedService<IngestionRecoveryWorker>();
+builder.Services.AddHostedService<WarApiPlannerWorker>();
+builder.Services.AddHostedService<WarApiExecutorWorker>();
 
 await builder.Build().RunAsync();
